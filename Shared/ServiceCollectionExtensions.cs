2638c06a4aedfec29ab87d37e5bc5da9cd7fbb0d
t-kuentzler <tobias.kuentzler@outlook.de>
2024-07-14 20:32:24 +0200

    Validators registriert

--- Shared/ServiceCollectionExtensions.cs
+++ Shared/ServiceCollectionExtensions.cs
@@ -1,8 +1,11 @@
+using FluentValidation;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Shared.Contracts;
+using Shared.Entities;
 using Shared.Factories;
+using Shared.Models;
 using Shared.Repositories;
 using Shared.Services;
 using Shared.Validation;
@@ -38,55 +41,19 @@ public static class ServiceCollectionExtensions
 
             
             // OAuth Client Service Factory
-            services.AddSingleton<OAuthClientServiceFactory>();
+            services.AddSingleton<IOAuthClientServiceFactory, OAuthClientServiceFactory>();
             services.AddScoped<IOAuthClientService>(provider =>
                 provider.GetRequiredService<OAuthClientServiceFactory>().Create());
             
             //Validator Wrapper
             services.AddTransient(typeof(IValidatorWrapper<>), typeof(ValidatorWrapper<>));
+            
+            //Validators
+            services.AddTransient<IValidator<AccessToken>, AccessTokenValidator>();
+            services.AddTransient<IValidator<DeliveryAddress>, DeliveryAddressValidator>();
+            services.AddTransient<IValidator<OrderEntry>, OrderEntryValidator>();
+            services.AddTransient<IValidator<Order>, OrderValidator>();
+            services.AddTransient<IValidator<UpdateStatus>, UpdateStatusValidator>();
 
-            
-            
-            // // Repository-Dienste
-            // services.AddScoped<IAccessTokenRepository, AccessTokenRepository>();
-            // services.AddScoped<IOrderRepository, OrderRepository>();
-            // services.AddScoped<IConsignmentRepository, ConsignmentRepository>();
-            // services.AddScoped<IReturnRepository, ReturnRepository>();
-            //
-            // // Anwendungs-Dienste
-            // services.AddScoped<IAccessTokenService, AccessTokenService>();
-            // services.AddScoped<IOAuthClientService, OAuthClientService>();
-            // services.AddScoped<IOrderService, OrderService>();
-            // services.AddScoped<IConsignmentService, ConsignmentService>();
-            // services.AddScoped<IReturnService, ReturnService>();
-            // services.AddScoped<ICsvFileService, CsvFileService>();
-            // services.AddScoped<IEmailService, EmailService>();
-            // services.AddScoped<IRmaNumberGenerator, RandomRmaNumberGenerator>();
-            //
-            // // FluentValidation Validators
-            // services.AddTransient<IValidator<AccessToken>, AccessTokenValidator>();
-            // services.AddTransient<IValidator<Order>, OrderValidator>();
-            // services.AddTransient<IValidator<OrderEntry>, OrderEntryValidator>();
-            // services.AddTransient<IValidator<DeliveryAddress>, DeliveryAddressValidator>();
-            // services.AddTransient<IValidator<UpdateStatus>, UpdateStatusValidator>();
-            // services.AddTransient<IValidator<CustomerInfo>, CustomerInfoValidator>();
-            // services.AddTransient<IValidator<ReturnConsignment>, ReturnConsignmentValidator>();
-            // services.AddTransient<IValidator<ReturnEntry>, ReturnEntryValidator>();
-            // services.AddTransient<IValidator<Return>, ReturnValidator>();
-            // services.AddTransient<IValidator<Address>, AddressValidator>();
-            // services.AddTransient<IValidator<ReturnPackage>, ReturnPackageValidator>();
-            // services.AddTransient<IValidator<Consignment>, ConsignmentValidator>();
-            // services.AddTransient<IValidator<ConsignmentEntry>, ConsignmentEntryValidator>();
-            // services.AddTransient<IValidator<ShippingAddress>, ShippingAddressValidator>();
-            //
-            // // Konfigurationseinstellungen
-            // services.Configure<OAuthSettings>(configuration.GetSection("OAuthSettings"));
-            // services.Configure<EmailConfiguration>(configuration.GetSection("EmailConfiguration"));
-            //
-            // // OAuth Client Service Factory
-            // services.AddSingleton<OAuthClientServiceFactory>();
-            // services.AddScoped<IOAuthClientService>(provider =>
-            //     provider.GetRequiredService<OAuthClientServiceFactory>().Create());
-            // services.AddSingleton<SmtpClient>();
         }
     }
