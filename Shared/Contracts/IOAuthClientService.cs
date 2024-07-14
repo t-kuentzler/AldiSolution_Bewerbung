866333e851e7d7e8d56ffb0f7408e65884bbf660
t-kuentzler <tobias.kuentzler@outlook.de>
2024-07-14 17:50:52 +0200

    Initial

--- Shared/Contracts/IOAuthClientService.cs
+++ Shared/Contracts/IOAuthClientService.cs
@@ -1,8 +1,15 @@
+using Shared.Entities;
 using Shared.Models;
 
 namespace Shared.Contracts;
 
 public interface IOAuthClientService
 {
+    Task<OAuthTokenResponse?> GetApiTokenAsync();
     Task<OrderResponse> GetApiOrdersAsync();
-}
+    Task<bool> CancelOrderEntriesAsync(string orderCode, IEnumerable<OrderCancellationEntry> cancellationEntries);
+    Task<(bool, ManualReturnResponse)> CreateManualReturnAsync(ManualReturnRequest manualReturnRequest);
+
+    Task<(bool, ReceivingReturnResponse)> CreateReceivingReturn(ReceivingReturnRequest parsedReceivingReturnRequest);
+    Task<bool> CancelConsignmentAfterDispatchAsync(Consignment consignment);
+    Task<bool> ReportReturnPackage(ReportReturnPackageRequest reportReturnPackageRequest);}
---
+++ Shared/Models/UpdateStatus.cs
@@ -0,0 +1,9 @@
+﻿namespace Shared.Models
+{
+    public class UpdateStatus
+    {
+        public string Code { get; set; } = default!;
+        public string Status { get; set; } = default!;
+
+    }
+}
