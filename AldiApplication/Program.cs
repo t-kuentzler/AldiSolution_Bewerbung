280663667d8a3ea8e24eb405526aae4d7d0062bc
t-kuentzler <tobias.kuentzler@outlook.de>
2024-07-14 20:01:52 +0200

    using System.Net.Mail entfernt

--- AldiApplication/Program.cs
+++ AldiApplication/Program.cs
@@ -1,5 +1,4 @@
-﻿using System.Net.Mail;
-using Microsoft.Extensions.Configuration;
+﻿using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
