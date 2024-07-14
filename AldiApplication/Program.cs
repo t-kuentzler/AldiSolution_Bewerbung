2a6a8878b4c9b07f773f04236d7aad449f3ed28c
t-kuentzler <tobias.kuentzler@outlook.de>
2024-07-14 16:35:07 +0200

    CheckDatabase angepasst

--- AldiApplication/Program.cs
+++ AldiApplication/Program.cs
@@ -1,8 +1,81 @@
-﻿namespace AldiApplication;
+﻿using System.Net.Mail;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+using Serilog;
+using Shared;
+using Shared.Logger;
 
-class Program
+namespace AldiApplication
 {
-    static void Main(string[] args)
+    public class Program
     {
+        public static async Task Main(string[] args)
+        {
+            // Logger konfigurieren
+            LoggerConfigurator.ConfigureLogger();
+
+            var host = CreateHostBuilder(args).Build();
+
+            var logger = host.Services.GetRequiredService<ILogger<Program>>();
+            logger.LogInformation("Die Anwendung wird gestartet...");
+
+            await CheckDatabaseConnection(host);
+
+            // await ExecuteOrderProcessAsync(host.Services);
+            // await ReadAndSaveConsignmentsAsync(host.Services);
+            // await ReadAndSaveReturnsAsync(host.Services);
+
+            Log.CloseAndFlush();
+        }
+
+        private static async Task CheckDatabaseConnection(IHost host)
+        {
+            using (var scope = host.Services.CreateScope())
+            {
+                var services = scope.ServiceProvider;
+                try
+                {
+                    var dbContext = services.GetRequiredService<ApplicationDbContext>();
+                    var canConnect = await dbContext.Database.CanConnectAsync();
+                    if (canConnect)
+                    {
+                        Log.Information("Verbindung zur Datenbank erfolgreich.");
+                    }
+                    else
+                    {
+                        Log.Error("Fehler bei der Verbindung zur Datenbank.");
+                    }
+                }
+                catch (Exception ex)
+                {
+                    Log.Error(ex, "Ein unerwarteter Fehler ist aufgetreten beim Versuch, die Datenbankverbindung zu prüfen.");
+                }
+            }
+        }
+
+        private static IHostBuilder CreateHostBuilder(string[] args) =>
+            Host.CreateDefaultBuilder(args)
+                .ConfigureAppConfiguration((context, builder) =>
+                {
+                    var sharedConfigPath = Path.Combine(AppContext.BaseDirectory, "appsettings.json");
+
+                    builder.AddJsonFile(sharedConfigPath, optional: false, reloadOnChange: true)
+                           .AddJsonFile("appsettings.json", optional: true, reloadOnChange: true)
+                           .AddEnvironmentVariables();
+                })
+                .ConfigureServices((context, services) => { ConfigureServices(services, context.Configuration); })
+                .UseSerilog()  // Serilog verwenden
+                .ConfigureLogging(logging =>
+                {
+                    logging.ClearProviders();
+                    logging.AddSerilog();
+                });
+
+        private static void ConfigureServices(IServiceCollection services, IConfiguration configuration)
+        {
+            services.AddSharedServices(configuration);
+        }
     }
 }
