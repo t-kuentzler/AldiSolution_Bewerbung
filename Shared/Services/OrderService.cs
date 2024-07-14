e402022bca04df843bdc4ee917edfecf6eb847fb
t-kuentzler <tobias.kuentzler@outlook.de>
2024-07-14 20:12:41 +0200

    ProcessSingleOrderAsync überarbeitet

--- Shared/Services/OrderService.cs
+++ Shared/Services/OrderService.cs
@@ -19,7 +19,7 @@ public class OrderService : IOrderService
 
 
     public OrderService(IAccessTokenService accessTokenService, IOAuthClientService oAuthClientService,
-        ILogger<OrderService> logger, IValidatorWrapper<Order> orderValidator, 
+        ILogger<OrderService> logger, IValidatorWrapper<Order> orderValidator,
         IValidatorWrapper<UpdateStatus> updateStatusValidator, IOrderRepository orderRepository)
     {
         _accessTokenService = accessTokenService;
@@ -29,7 +29,7 @@ public class OrderService : IOrderService
         _updateStatusValidator = updateStatusValidator;
         _orderRepository = orderRepository;
     }
-    
+
     public async Task ProcessOpenOrdersAsync()
     {
         try
@@ -68,10 +68,11 @@ public class OrderService : IOrderService
         }
         catch (Exception ex)
         {
-            _logger.LogError($"Ein unerwarteter Fehler ist bei der Verarbeitung der Bestellung '{order.Code}' aufgetreten: {ex.Message}");
+            _logger.LogError(
+                $"Ein unerwarteter Fehler ist bei der Verarbeitung der Bestellung '{order.Code}' aufgetreten: {ex.Message}");
         }
     }
-    
+
     public async Task<Order> GetOrderByOrderCodeAsync(string orderCode)
     {
         if (string.IsNullOrEmpty(orderCode))
@@ -106,7 +107,7 @@ public class OrderService : IOrderService
                 $"Unerwarteter Fehler beim Abrufen von Order mit dem OrderCode '{orderCode}'.", ex);
         }
     }
-    
+
     private async Task AddOrderAsync(Order order)
     {
         _logger.LogInformation("Es wird versucht, eine Bestellung in der Datenbank zu erstellen.");
@@ -116,69 +117,56 @@ public class OrderService : IOrderService
             _logger.LogError("Die zu erstellende Bestellung ist null.");
             throw new ArgumentNullException(nameof(order), "Die zu erstellende Bestellung ist null.");
         }
-            
+
         try
         {
             await _orderValidator.ValidateAndThrowAsync(order);
             await _orderRepository.CreateOrderAsync(order);
-            _logger.LogInformation($"Die Bestellung mit dem OrderCode '{order.Code}' wurde erfolgreich in der Datenbank erstellt.");
+            _logger.LogInformation(
+                $"Die Bestellung mit dem OrderCode '{order.Code}' wurde erfolgreich in der Datenbank erstellt.");
         }
         catch (ValidationException ex)
         {
-            _logger.LogError(ex, $"Bei der Bestellung mit dem Code '{order.Code}' ist ein Validierungsfehler aufgetreten: {ex.Message}");
-            throw; 
+            _logger.LogError(ex,
+                $"Bei der Bestellung mit dem Code '{order.Code}' ist ein Validierungsfehler aufgetreten: {ex.Message}");
+            throw;
         }
         catch (Exception ex)
         {
             var createOrderException = new CreateOrderException(order.Code, ex);
             _logger.LogError(createOrderException.Message);
-            throw; 
+            throw;
         }
     }
-    
-    private async Task<bool> UpdateOrderStatusInDatabaseAsync(Order? order, string status)
+
+    private async Task UpdateOrderStatusInDatabaseAsync(Order? order, string status)
     {
         if (order == null)
         {
             _logger.LogError("Die übergebene Bestellung ist null.");
-            return false;
+            throw new OrderIsNullException();
         }
 
-        try
+        var updateStatus = new UpdateStatus
         {
-            var updateStatus = new UpdateStatus
-            {
-                Code = order.Code,
-                Status = status
-            };
-            await _updateStatusValidator.ValidateAndThrowAsync(updateStatus);
-        }
-        catch (ValidationException ex)
-        {
-            _logger.LogError($"Bei der Aktualisierung des Status {status} mit dem Code {order.Code} ist ein Fehler aufgetreten: {ex.Message}");
-            return false; 
-        }
+            Code = order.Code,
+            Status = status
+        };
+        await _updateStatusValidator.ValidateAndThrowAsync(updateStatus);
 
-        try
-        {
-            bool updateSuccess = await _orderRepository.UpdateOrderStatusAsync(order.Code, status);
 
-            if (!updateSuccess)
-            {
-                _logger.LogError($"Fehler beim Aktualisieren des Status in der Datenbank für Bestellung mit Code {order.Code}.");
-                return false;
-            }
-            else
-            {
-                _logger.LogInformation($"Bestellung mit Code {order.Code} wurde erfolgreich in der Datenbank auf {status} aktualisiert.");
-                return true; 
-            }
-        }
-        catch (Exception ex)
+        bool updateSuccess = await _orderRepository.UpdateOrderStatusAsync(order.Code, status);
+
+        if (!updateSuccess)
         {
-            var updateOrderStatusException = new UpdateOrderStatusException(order.Code, ex);
-            _logger.LogError(updateOrderStatusException.Message);
-            return false; 
+            throw new UpdateDatabaseException(
+                $"Fehler beim Aktualisieren des Status in der Datenbank für Bestellung mit Code '{order.Code}'.");
+        }
+        else
+        {
+            _logger.LogInformation(
+                $"Bestellung mit Code '{order.Code}' wurde erfolgreich in der Datenbank auf '{status}' aktualisiert.");
         }
     }
 }
+
