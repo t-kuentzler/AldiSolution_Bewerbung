406ebbd2318b2aff9cc0b667c4be524c8fb5b02d
t-kuentzler <tobias.kuentzler@outlook.de>
2024-07-15 12:11:34 +0200

    Initial

--- Shared/Contracts/IConsignmentRepository.cs
+++ Shared/Contracts/IConsignmentRepository.cs
@@ -1,6 +1,12 @@
+using Shared.Entities;
+
 namespace Shared.Contracts;
 
 public interface IConsignmentRepository
 {
-    
+    Task<(bool success, int consignmentId)> SaveConsignmentAsync(Consignment consignment);
+    Task SaveConsignmentEntryAsync(ConsignmentEntry consignmentEntry);
+    Task UpdateConsignmentEntryAsync(ConsignmentEntry consignmentEntry);
+    Task<Consignment?> GetConsignmentByIdAsync(int consignmentId);
+    Task UpdateConsignmentAsync(Consignment consignment);
 }
