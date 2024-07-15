fb82b9b4e463f9fe53214f7137ccf6dbe0d8e285
t-kuentzler <tobias.kuentzler@outlook.de>
2024-07-15 15:52:19 +0200

    Initial

---
+++ Shared/Exceptions/GetConsignmentsWithStatusShippedException.cs
@@ -0,0 +1,24 @@
+﻿using System.Runtime.Serialization;
+
+namespace Shared.Exceptions
+{
+    [Serializable]
+    internal class GetConsignmentsWithStatusShippedException : Exception
+    {
+        public GetConsignmentsWithStatusShippedException()
+        {
+        }
+
+        public GetConsignmentsWithStatusShippedException(string? message) : base(message)
+        {
+        }
+
+        public GetConsignmentsWithStatusShippedException(string? message, Exception? innerException) : base(message, innerException)
+        {
+        }
+
+        protected GetConsignmentsWithStatusShippedException(SerializationInfo info, StreamingContext context) : base(info, context)
+        {
+        }
+    }
+}
