395fcb1572b43707501e1b4ab98e151571eeb918
t-kuentzler <tobias.kuentzler@outlook.de>
2024-07-15 13:52:14 +0200

    CreateReturnAsync erstellt

--- Shared/Contracts/IReturnRepository.cs
+++ Shared/Contracts/IReturnRepository.cs
@@ -1,6 +1,8 @@
+using Shared.Entities;
+
 namespace Shared.Contracts;
 
 public interface IReturnRepository
 {
-    
+    Task<bool> CreateReturnAsync(Return returnObj);
 }
--- Shared/Repositories/ReturnRepository.cs
+++ Shared/Repositories/ReturnRepository.cs
@@ -1,8 +1,30 @@
 using Shared.Contracts;
+using Shared.Entities;
+using Shared.Exceptions;
 
 namespace Shared.Repositories;
 
 public class ReturnRepository : IReturnRepository
 {
+    private readonly ApplicationDbContext _applicationDbContext;
+
+    public ReturnRepository(ApplicationDbContext applicationDbContext)
+    {
+        _applicationDbContext = applicationDbContext;
+    }
     
+    public async Task<bool> CreateReturnAsync(Return returnObj)
+    {
+        try
+        {
+            _applicationDbContext.Return.Add(returnObj);
+            await _applicationDbContext.SaveChangesAsync();
+            return true;
+        }
+        catch (Exception ex)
+        {
+            throw new RepositoryException(
+                $"Ein unerwarteter Fehler ist aufgetreten beim Erstellen der Retoure. Rma code: '{returnObj.Rma}', OrderCode: '{returnObj.OrderCode}', AldiReturnCode: '{returnObj.AldiReturnCode}'.", ex);
+        }
+    }
 }
