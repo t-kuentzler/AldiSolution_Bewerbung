afe326a23fdb3b66bbabf075755cd23604f25542
t-kuentzler <tobias.kuentzler@outlook.de>
2024-07-15 14:25:29 +0200

    ParseReturnToReturnInProgressRequest erstellt

--- Shared/Contracts/IReturnService.cs
+++ Shared/Contracts/IReturnService.cs
@@ -1,6 +1,11 @@
+using Shared.Entities;
+using Shared.Models;
+
 namespace Shared.Contracts;
 
 public interface IReturnService
 {
-    
+    List<Return> ParseReturnResponseToReturnObject(ReturnResponse returnResponse);
+    Task<bool> CreateReturnAsync(Return returnObj);
+    ReturnInProgressRequest? ParseReturnToReturnInProgressRequest(Return returnObj);
 }
