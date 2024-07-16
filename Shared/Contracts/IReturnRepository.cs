c700e49b180e2427af1029d28655a0b49cfd01fb
t-kuentzler <tobias.kuentzler@outlook.de>
2024-07-16 16:36:45 +0200

    SearchReturnsAsync erstellt

--- Shared/Contracts/IReturnRepository.cs
+++ Shared/Contracts/IReturnRepository.cs
@@ -1,8 +1,10 @@
 using Shared.Entities;
+using Shared.Models;
 
 namespace Shared.Contracts;
 
 public interface IReturnRepository
 {
     Task<bool> CreateReturnAsync(Return returnObj);
+    Task<List<Return>> SearchReturnsAsync(SearchTerm searchTerm, string status);
 }
--- Shared/Repositories/ReturnRepository.cs
+++ Shared/Repositories/ReturnRepository.cs
@@ -1,6 +1,8 @@
+using Microsoft.EntityFrameworkCore;
 using Shared.Contracts;
 using Shared.Entities;
 using Shared.Exceptions;
+using Shared.Models;
 
 namespace Shared.Repositories;
 
@@ -25,6 +27,28 @@ public class ReturnRepository : IReturnRepository
         {
             throw new RepositoryException(
                 $"Ein unerwarteter Fehler ist aufgetreten beim Erstellen der Retoure. Rma code: '{returnObj.Rma}', OrderCode: '{returnObj.OrderCode}', AldiReturnCode: '{returnObj.AldiReturnCode}'.", ex);
+        }
+    }
+    
+    public async Task<List<Return>> SearchReturnsAsync(SearchTerm searchTerm, string status)
+    {
+        try
+        {
+            return await _applicationDbContext.Return
+                .Include(r => r.CustomerInfo)
+                .ThenInclude(entry => entry.Address)
+                .Include(r => r.ReturnEntries)
+                .ThenInclude(entry => entry.ReturnConsignments)
+                .ThenInclude(c => c.Packages)
+                .Where(r => r.Status == status
+                            && (r.OrderCode.Contains(searchTerm.value)
+                                || r.Rma.Contains(searchTerm.value))).ToListAsync();
+        }
+        catch (Exception ex)
+        {
+            throw new RepositoryException(
+                $"Ein unerwarteter Fehler ist aufgetreten beim Suchen von Retouren. Suchbegriff: '{searchTerm.value}', Status: '{status}'",
+                ex);
         }
     }
 }
