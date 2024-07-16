4a759ca139e50b5f079124a3a996152791933405
t-kuentzler <tobias.kuentzler@outlook.de>
2024-07-16 15:19:34 +0200

    Initial

---
+++ AldiOrderManagement/Views/Order/DownloadConfirmation.cshtml.cs
@@ -0,0 +1,11 @@
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace AldiOrderManagement.Views;
+
+public class DownloadConfirmation : PageModel
+{
+    public void OnGet()
+    {
+        
+    }
+}
---
+++ AldiOrderManagement/Views/Return/ReturnDetails.cshtml.cs
@@ -0,0 +1,11 @@
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace AldiOrderManagement.Views.Return;
+
+public class ReturnDetails : PageModel
+{
+    public void OnGet()
+    {
+        
+    }
+}
