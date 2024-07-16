58a38aecff1e29ae36c534c2b07c3fb2d6b48a21
t-kuentzler <tobias.kuentzler@outlook.de>
2024-07-16 09:48:04 +0200

    Initial

---
+++ Shared/Models/TrackingData.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Shared.Models
+{
+    public class TrackingData
+    {
+        public string pushid { get; set; } = default!;
+        [FromQuery(Name = "ref")]
+        public string? reference { get; set; }
+
+        public string? pnr { get; set; } = default!;
+        public string? depot { get; set; }
+        public string? status { get; set; } = default!;
+        public string? statusdate { get; set; }
+        public string? weight { get; set; }
+        public string? receiver { get; set; }
+        public string? pod { get; set; }
+        public string? scaninfo { get; set; }
+        public string? services { get; set; }
+    }
+}
