1ea1866786151ad181e4f114175e2077a86fab0d
t-kuentzler <tobias.kuentzler@outlook.de>
2024-07-17 10:22:08 +0200

    AddAntiforgery erstellt

--- AldiOrderManagement/Program.cs
+++ AldiOrderManagement/Program.cs
@@ -1,52 +1,94 @@
 using Microsoft.AspNetCore.Identity;
-using Microsoft.EntityFrameworkCore;
-using AldiOrderManagement.Data;
+using Serilog;
+using Shared;
+using Shared.Logger;
+using ApplicationDbContext = AldiOrderManagement.Data.ApplicationDbContext;
+using Microsoft.AspNetCore.Antiforgery;
+using Microsoft.AspNetCore.DataProtection;
+using System.Runtime.InteropServices;
 
-namespace AldiOrderManagement;
-
-public class Program
+namespace AldiOrderManagement
 {
-    public static void Main(string[] args)
+    public class Program
     {
-        var builder = WebApplication.CreateBuilder(args);
-
-        // Add services to the container.
-        var connectionString = builder.Configuration.GetConnectionString("DefaultConnection") ??
-                               throw new InvalidOperationException("Connection string 'DefaultConnection' not found.");
-        builder.Services.AddDbContext<ApplicationDbContext>(options =>
-            options.UseSqlite(connectionString));
-        builder.Services.AddDatabaseDeveloperPageExceptionFilter();
-
-        builder.Services.AddDefaultIdentity<IdentityUser>(options => options.SignIn.RequireConfirmedAccount = true)
-            .AddEntityFrameworkStores<ApplicationDbContext>();
-        builder.Services.AddControllersWithViews();
-
-        var app = builder.Build();
-
-        // Configure the HTTP request pipeline.
-        if (app.Environment.IsDevelopment())
+        public static void Main(string[] args)
         {
-            app.UseMigrationsEndPoint();
+            LoggerConfigurator.ConfigureLogger();
+
+            Log.Information("Die Anwendung wurde gestartet.");
+
+            var builder = WebApplication.CreateBuilder(args);
+
+            // Load shared configurations
+            var sharedConfigPath = Path.Combine(AppContext.BaseDirectory, "appsettings.json");
+
+            builder.Configuration
+                .AddJsonFile(sharedConfigPath, optional: false, reloadOnChange: true)
+                .AddJsonFile("appsettings.json", optional: true, reloadOnChange: true)
+                .AddEnvironmentVariables();
+
+            // Add services to the container.
+            builder.Services.AddDefaultIdentity<IdentityUser>(options => options.SignIn.RequireConfirmedAccount = true)
+                .AddEntityFrameworkStores<ApplicationDbContext>();
+            builder.Services.AddControllersWithViews();
+            builder.Services.AddAntiforgery(options => options.HeaderName = "X-XSRF-TOKEN");
+
+            // Data Protection Configuration
+            if (RuntimeInformation.IsOSPlatform(OSPlatform.Windows))
+            {
+                // Konfiguration für Windows
+                builder.Services.AddDataProtection()
+                    .PersistKeysToFileSystem(new DirectoryInfo(@"\\10.4.240.76\aspnet_share\antiforgerytoken\AldiOrderManagement_TEST"))
+                    .SetApplicationName("AldiOrderManagement_TEST");
+            }
+            else if (RuntimeInformation.IsOSPlatform(OSPlatform.OSX))
+            {
+                // Konfiguration für macOS
+                builder.Services.AddDataProtection()
+                    .PersistKeysToFileSystem(new DirectoryInfo("/Volumes/aspnet_share/antiforgerytoken/AldiOrderManagement_TEST"))
+                    .SetApplicationName("AldiOrderManagement_TEST");
+            }
+
+            // Register shared services
+            builder.Services.AddSharedServices(builder.Configuration);
+
+            var app = builder.Build();
+
+            // Configure the HTTP request pipeline.
+            if (app.Environment.IsDevelopment())
+            {
+                app.UseMigrationsEndPoint();
+            }
+            else
+            {
+                app.UseExceptionHandler("/Home/Error");
+                // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+                app.UseHsts();
+            }
+
+            app.UseHttpsRedirection();
+            app.UseStaticFiles();
+
+            app.UseRouting();
+
+            app.UseAuthentication();
+            app.UseAuthorization();
+
+            // Add Anti-Forgery Middleware
+            app.Use((context, next) =>
+            {
+                var tokens = context.RequestServices.GetService<IAntiforgery>().GetAndStoreTokens(context);
+                context.Response.Cookies.Append("XSRF-TOKEN", tokens.RequestToken,
+                    new CookieOptions { HttpOnly = false });
+                return next();
+            });
+
+            app.MapControllerRoute(
+                name: "default",
+                pattern: "{controller=Home}/{action=Index}/{id?}");
+            app.MapRazorPages();
+
+            app.Run();
         }
-        else
-        {
-            app.UseExceptionHandler("/Home/Error");
-            // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
-            app.UseHsts();
-        }
-
-        app.UseHttpsRedirection();
-        app.UseStaticFiles();
-
-        app.UseRouting();
-
-        app.UseAuthorization();
-
-        app.MapControllerRoute(
-            name: "default",
-            pattern: "{controller=Home}/{action=Index}/{id?}");
-        app.MapRazorPages();
-
-        app.Run();
     }
 }
