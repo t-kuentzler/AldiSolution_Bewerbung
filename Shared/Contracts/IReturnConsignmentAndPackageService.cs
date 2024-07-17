956d667b88487009a72213af2a9f8b6874fd8a6e
t-kuentzler <tobias.kuentzler@outlook.de>
2024-07-17 09:34:18 +0200

    Code kopiert

--- Shared/Contracts/IReturnConsignmentAndPackageService.cs
+++ Shared/Contracts/IReturnConsignmentAndPackageService.cs
@@ -1,6 +1,18 @@
+using Shared.Entities;
+using Shared.Models;
+
 namespace Shared.Contracts;
 
 public interface IReturnConsignmentAndPackageService
 {
-    
+    Task UpdateAllReturnPackageStatusFromReturnAsync(string packageStatus, Return returnObj);
+    Task UpdateReturnConsignmentStatusQuantityAsync(string packageStatus, Return returnObj);
+    Task UpdateCompletedDateForAllReturnConsignments(Return returnObj);
+    Task UpdateReturnConsignmentAsync(ReturnConsignment returnConsignment);
+
+    Task<Return> CreateReturnConsignmentAndReturnPackage(
+        ReceivingReturnResponse parsedReceivingReturnResponse,
+        Return returnObj);
+
+    Task<ReturnConsignment> GetReturnConsignmentByConsignmentCodeAsync(string consignmentCode);
 }
