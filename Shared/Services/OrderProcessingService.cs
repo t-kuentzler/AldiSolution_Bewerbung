3bd3be30e56bdcf9bf45bdd871b943c200b58fd7
t-kuentzler <tobias.kuentzler@outlook.de>
2024-07-27 23:42:57 +0200

    Namespace angepasst

--- Shared/Services/OrderProcessingService.cs
+++ Shared/Services/OrderProcessingService.cs
@@ -1,9 +1,7 @@
-using System;
-using System.Threading.Tasks;
 using Microsoft.Extensions.Logging;
 using Shared.Contracts;
 
-namespace Shared
+namespace Shared.Services
 {
     public class OrderProcessingService : IOrderProcessingService
     {
