047108af3d371f6309f138a6cee429129b28bcad
t-kuentzler <tobias.kuentzler@outlook.de>
2024-07-29 10:59:41 +0200

    ISmtpClientWrapper eingebaut

--- Shared/Services/EmailService.cs
+++ Shared/Services/EmailService.cs
@@ -1,64 +1,59 @@
-using System.Net;
 using System.Net.Mail;
 using Microsoft.Extensions.Logging;
 using Microsoft.Extensions.Options;
 using Shared.Contracts;
 using Shared.Models;
 
-namespace Shared.Services
+namespace Shared.Services;
+
+public class EmailService : IEmailService
 {
-    public class EmailService : IEmailService
+    private readonly ILogger<EmailService> _logger;
+    private readonly ISmtpClientWrapper _smtpClientWrapper;
+    private readonly EmailConfiguration _emailConfiguration;
+
+    public EmailService(ILogger<EmailService> logger, ISmtpClientWrapper smtpClientWrapper,
+        IOptions<EmailConfiguration> emailConfiguration)
     {
-        private readonly ILogger<EmailService> _logger;
-        private readonly SmtpClient _smtpClient;
-        private readonly EmailConfiguration _emailConfiguration;
+        _logger = logger;
+        _emailConfiguration = emailConfiguration.Value ?? throw new ArgumentNullException(nameof(emailConfiguration));
 
-        public EmailService(ILogger<EmailService> logger, SmtpClient smtpClient, IOptions<EmailConfiguration> emailConfiguration)
+        if (string.IsNullOrEmpty(_emailConfiguration.SmtpServer))
+            throw new ArgumentNullException(nameof(_emailConfiguration.SmtpServer));
+        if (string.IsNullOrEmpty(_emailConfiguration.SenderEmail))
+            throw new ArgumentNullException(nameof(_emailConfiguration.SenderEmail));
+        if (string.IsNullOrEmpty(_emailConfiguration.SenderUsername))
+            throw new ArgumentNullException(nameof(_emailConfiguration.SenderUsername));
+        if (string.IsNullOrEmpty(_emailConfiguration.SenderPassword))
+            throw new ArgumentNullException(nameof(_emailConfiguration.SenderPassword));
+        if (string.IsNullOrEmpty(_emailConfiguration.RecipientEmail))
+            throw new ArgumentNullException(nameof(_emailConfiguration.RecipientEmail));
+
+        _smtpClientWrapper = smtpClientWrapper;
+    }
+
+    public async Task SendReturnNotificationEmailAsync()
+    {
+        try
         {
-            _logger = logger;
-            _emailConfiguration = emailConfiguration.Value ?? throw new ArgumentNullException(nameof(emailConfiguration));
+            var subject = "Aldi Retouren";
+            var body = "Es wurden neue Retouren im Aldi Portal abgerufen.";
 
-            if (string.IsNullOrEmpty(_emailConfiguration.SmtpServer))
-                throw new ArgumentNullException(nameof(_emailConfiguration.SmtpServer));
-            if (string.IsNullOrEmpty(_emailConfiguration.SenderEmail))
-                throw new ArgumentNullException(nameof(_emailConfiguration.SenderEmail));
-            if (string.IsNullOrEmpty(_emailConfiguration.SenderUsername))
-                throw new ArgumentNullException(nameof(_emailConfiguration.SenderUsername));
-            if (string.IsNullOrEmpty(_emailConfiguration.SenderPassword))
-                throw new ArgumentNullException(nameof(_emailConfiguration.SenderPassword));
-            if (string.IsNullOrEmpty(_emailConfiguration.RecipientEmail))
-                throw new ArgumentNullException(nameof(_emailConfiguration.RecipientEmail));
+            using (var message = new MailMessage())
+            {
+                message.From = new MailAddress(_emailConfiguration.SenderEmail);
+                message.To.Add(_emailConfiguration.RecipientEmail);
+                message.Subject = subject;
+                message.Body = body;
 
-            _smtpClient = smtpClient;
-            _smtpClient.Host = _emailConfiguration.SmtpServer;
-            _smtpClient.Port = _emailConfiguration.Port;
-            _smtpClient.UseDefaultCredentials = false;
-            _smtpClient.Credentials = new NetworkCredential(_emailConfiguration.SenderUsername, _emailConfiguration.SenderPassword);
+                await _smtpClientWrapper.SendMailAsync(message);
+
+                _logger.LogInformation("Die Email für die Retoureninformation wurde erfolgreich versendet.");
+            }
         }
-        
-        public async Task SendReturnNotificationEmailAsync()
+        catch (Exception ex)
         {
-            try
-            {
-                var subject = "Aldi Retouren";
-                var body = "Es wurden neue Retouren im Aldi Portal abgerufen.";
-
-                using (var message = new MailMessage())
-                {
-                    message.From = new MailAddress(_emailConfiguration.SenderEmail);
-                    message.To.Add(_emailConfiguration.RecipientEmail);
-                    message.Subject = subject;
-                    message.Body = body;
-
-                    await _smtpClient.SendMailAsync(message); 
-
-                    _logger.LogInformation("Die Email für die Retoureninformation wurde erfolgreich versendet.");
-                }
-            }
-            catch (Exception ex)
-            {
-                _logger.LogError(ex, "Fehler beim Senden der Retoureninformations Email.");
-            }
+            _logger.LogError(ex, "Fehler beim Senden der Retoureninformations Email.");
         }
     }
 }
